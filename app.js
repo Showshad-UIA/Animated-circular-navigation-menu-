7535cf5b119c1b880ca220285aab2c0a0b79c07b
raihan <raihanulmcse@mail.com>
2022-03-05 04:40:44 +0800

    additional part added

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ menuToggle.onclick = function () {
 	menu.classList.toggle("active");
 	menuToggle.classList.toggle("active");
 };
-const li = document.querySelectorAll("li");
+const list = document.querySelectorAll("li");
 function activeLink() {
-	list.forEach((item) => item.classList.remove(active));
+	list.forEach((item) => item.classList.remove("active"));
 	this.classList.add("active");
 }
+list.forEach((item) => item.addEventListener("click", activeLink));
