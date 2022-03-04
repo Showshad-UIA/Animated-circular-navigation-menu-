4243b458674ebd54d33f71a3effeed46ed7d858e
raihan <raihanulmcse@mail.com>
2022-03-05 03:03:31 +0800

    CSS added

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ menuToggle.onclick = function () {
 	menu.classList.toggle("active");
 	menuToggle.classList.toggle("active");
 };
+const li = document.querySelectorAll("li");
+function activeLink() {
+	list.forEach((item) => item.classList.remove(active));
+	this.classList.add("active");
+}
